8886671c9b60e991043d863886062a4ec6c13389
joaogl <joaoguerralourenco@hotmail.com>
2013-06-13 12:43:51 +0100

    Update.

--- Acars/src/com/acars/main/MainMethod.java
+++ Acars/src/com/acars/main/MainMethod.java
@@ -90,10 +90,6 @@ public class MainMethod {
 	public static void main(String[] args) {
 		System.out.println("Running tests");
 		System.out.println("T1");
-		FSFlightSim sim = new FSFlightSim();
-		System.out.println("T2");
-		System.out.println("name " + sim.StartSituationName() );
-		System.out.println("T3");
 		int ret = fsuipc_wrapper.Open(fsuipc_wrapper.SIM_ANY);
 		System.out.println("ret =" + ret);
 		if (ret == 0) {
